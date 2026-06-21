97cb51a78768bd711059526074db348b4340df69
aynny <ayny475@gmail.com>
2026-06-21 13:27:13 +0900

    Configure Supabase auth

--- src/auth-config.js
+++ src/auth-config.js
@@ -1,4 +1,4 @@
 export const AUTH_CONFIG = {
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://smotjjmnukhuzlvvjkxy.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Pdd5I7VTtDOr2dF_Y4mEHg_t_n9ucLU'
 };
