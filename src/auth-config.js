a27566d10da726909504a538bc93d2557d40986d
aynny <ayny475@gmail.com>
2026-06-25 18:06:14 +0900

    Configure Logo.dev publishable key

--- src/auth-config.js
+++ src/auth-config.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   supabaseUrl: 'https://smotjjmnukhuzlvvjkxy.supabase.co',
   supabaseAnonKey: 'sb_publishable_Pdd5I7VTtDOr2dF_Y4mEHg_t_n9ucLU',
-  logoDevApiKey: ''
+  logoDevApiKey: 'pk_NKVwAjJZSbSH5CXfccZtuw'
 };
